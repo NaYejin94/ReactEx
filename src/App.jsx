ad7c4f2c9b6e322242dc7c08a66151ac07e4791b
nayejin <nayejin259@gmail.com>
2025-02-11 21:33:43 +0900

    button 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
+import Board from "./Board";
 import { useState } from 'react';
-import Dice from './Dice'
 import Button from './Button'
+import './App.css';
 
 function random(n) {
     return Math.ceil(Math.random()*n);
 }
 
 function App() {
-    const [num, setNum] = useState(1);
+    const [myHistory, setMyHistory] = useState([]);
+    const [otherHistory, setOtherHistory] = useState([]);
 
     const handleRollClick = () => {
-        const nextNum = random(6)
-        setNum(nextNum);
+        const nextMyNum = random(6)
+        const nextOtherNum = random(6);
+        
+        setMyHistory([...myHistory,nextMyNum]);
+
+        setOtherHistory([...otherHistory,nextOtherNum]);
     }
 
     const handleClearClick = () => {
-        setNum(1);
+        setMyHistory([]);
+        setOtherHistory([]);
     }
 
     return (
-        <>
-            <div>주사위 굴리기!</div>
-            <div><Dice color='red' num={num}/></div>
+        <div className="App">
             <div>
-                <Button onClick={handleRollClick}>던지기</Button>
-                <Button onClick={handleClearClick}>처음부터</Button>
+                <Button className="App-button" color='blue' onClick={handleRollClick}>던지기</Button>
+                <Button className="App-button" color='red' onClick={handleClearClick}>처음부터</Button>
             </div>
-        </>
+            <div>
+                <Board name="나" color="blue" gameHistory={myHistory} />
+                <Board name="상대" color="red" gameHistory={otherHistory} />
+            </div>
+        </div>
     )
 }
 
